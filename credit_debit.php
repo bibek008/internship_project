1933a7eee8467c66c7dc1d175468d9d99b939875
unknown <bikendrathapaliya>
2019-03-18 16:27:09 +0530

    updated

--- credit_debit.php
+++ credit_debit.php
@@ -4,6 +4,7 @@ include("db_connect.php");
 	$card_no = $_POST['number'];
 	$ccv = $_POST['security-code'];
 	$date = $_POST['expiration-month-and-year'];
+	echo $card_no, $ccv, $date;
 	if(isset($name)&&isset($card_no)&&isset($ccv)){	
 			$sql0 = "INSERT INTO credit_debit (name, card_no, ccv, date)VALUES ('$name', '$card_no', '$ccv', '$date')";
 			if (mysqli_query($conn, $sql0)) {
--- internship_project.sql
+++ internship_project.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 18, 2019 at 11:47 AM
+-- Generation Time: Mar 18, 2019 at 11:56 AM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 7.1.1
 
@@ -29,17 +29,10 @@ SET time_zone = "+00:00";
 CREATE TABLE `credit_debit` (
   `id` int(11) NOT NULL,
   `name` varchar(60) NOT NULL,
-  `card_no` int(11) NOT NULL,
-  `ccv` int(11) NOT NULL,
-  `date` int(11) NOT NULL
+  `card_no` varchar(30) NOT NULL,
+  `ccv` varchar(10) NOT NULL,
+  `date` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `credit_debit`
---
-
-INSERT INTO `credit_debit` (`id`, `name`, `card_no`, `ccv`, `date`) VALUES
-(1, 'bibek', 1213, 122, 12);
 
 --
 -- Indexes for dumped tables
@@ -59,7 +52,7 @@ ALTER TABLE `credit_debit`
 -- AUTO_INCREMENT for table `credit_debit`
 --
 ALTER TABLE `credit_debit`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
