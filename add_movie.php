33bbf150293cc17c43e947e072782c54f0f10127
unknown <bikendrathapaliya>
2019-03-22 13:04:08 +0530

    a

--- add_movie.php
+++ add_movie.php
@@ -3,7 +3,7 @@
 <html>
 <head>
 <title>Insert a Movie</title>
-<link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
+<link href="css/style_add_movie.css" rel="stylesheet" type="text/css" media="all"/>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
@@ -20,9 +20,9 @@
 			<div class="main-content">
 				<div class="contact-w3">
 
-					<form action="#" method="post">
+					<form action="add_movie.php" method="post">
 						<label>Name</label>
-						<input type="text" name="Name" placeholder="Your name" required>
+						<input type="text" name="Name" placeholder="Movie name" required>
 						<div class="row">
 							<div class="contact-left-w3">
 								<label>Language</label>
@@ -39,7 +39,7 @@
 							
 						<div class="row1">
 							<label>Cast</label>
-							<textarea placeholder="Cast" name=""></textarea>
+							<textarea placeholder="Cast" name="Cast"></textarea>
 						</div>
 						<input type="submit" value="ADD">
 					</form>
@@ -49,3 +49,22 @@
 		
 </body>
 </html>
+<?php
+include("db_connect.php");
+	$Name = $_POST['Name'];
+	$Language = $_POST['Language'];
+	$Director = $_POST['Director'];
+	$Genre = $_POST['Genre'];
+	$Cast = $_POST['Cast'];
+	if(isset($Name)){	
+			$sql0 = "INSERT INTO movies (name, language, director, genre, cast)VALUES ('$Name', '$Language', '$Director', '$Genre', '$Cast')";
+			if (mysqli_query($conn, $sql0)) {
+				echo "ADDED MOVIES";
+				echo "<script >alert('movies added');</script>";
+			} /*else {
+				echo "Fail: " . $sql0 . "<br/>" . mysqli_error($conn);
+			}
+		*/
+	}
+mysqli_close($conn);
+?>
--- internship_project.sql
+++ internship_project.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 18, 2019 at 11:56 AM
+-- Generation Time: Mar 22, 2019 at 08:33 AM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 7.1.1
 
@@ -34,6 +34,40 @@ CREATE TABLE `credit_debit` (
   `date` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `movies`
+--
+
+CREATE TABLE `movies` (
+  `id` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `language` varchar(30) NOT NULL,
+  `director` varchar(30) NOT NULL,
+  `genre` varchar(30) NOT NULL,
+  `cast` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `seats`
+--
+
+CREATE TABLE `seats` (
+  `id` int(11) NOT NULL,
+  `seat_no` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `seats`
+--
+
+INSERT INTO `seats` (`id`, `seat_no`) VALUES
+(1, '1_2'),
+(2, '1_3');
+
 --
 -- Indexes for dumped tables
 --
@@ -45,6 +79,18 @@ ALTER TABLE `credit_debit`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `movies`
+--
+ALTER TABLE `movies`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `seats`
+--
+ALTER TABLE `seats`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -52,6 +98,16 @@ ALTER TABLE `credit_debit`
 -- AUTO_INCREMENT for table `credit_debit`
 --
 ALTER TABLE `credit_debit`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `movies`
+--
+ALTER TABLE `movies`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+--
+-- AUTO_INCREMENT for table `seats`
+--
+ALTER TABLE `seats`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
